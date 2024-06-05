35235fe7ac1f80cddae7fb70b9b2faaa593d4bb7
nineneel <138206811+sujarniel@users.noreply.github.com>
2024-06-05 17:41:16 +0700

    05/06/2024 finish LP GIP Japan 2024

--- 2024-japan/tailwind.config.js
+++ 2024-japan/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        background: "../asset/img/background.png",
+      },
+      colors: {
+        primary: "#0000FF",
+      },
+    },
   },
   plugins: [],
   prefix: "tw-",
